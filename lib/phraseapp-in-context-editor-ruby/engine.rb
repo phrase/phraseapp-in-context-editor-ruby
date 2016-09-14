e458b2462b9207c96d5a01c247232ea35138395d
docstun <manuel.boy@dynport.de>
2016-09-14 16:45:27 +0200

    Always load adapters to allow toggling In-Context editor during runtime

--- lib/phraseapp-in-context-editor-ruby/engine.rb
+++ lib/phraseapp-in-context-editor-ruby/engine.rb
@@ -7,10 +7,8 @@ if defined? Rails
     module InContextEditor
       class Engine < Rails::Engine
         initializer 'phraseapp-in-context-editor-ruby', before: :disable_dependency_loading do |app|
-          if PhraseApp::InContextEditor.enabled?
-            require 'phraseapp-in-context-editor-ruby/adapters/i18n'
-            require 'phraseapp-in-context-editor-ruby/adapters/fast_gettext'
-          end
+          require 'phraseapp-in-context-editor-ruby/adapters/i18n'
+          require 'phraseapp-in-context-editor-ruby/adapters/fast_gettext'
 
           ActionView::Base.send :include, PhraseApp::InContextEditor::ViewHelpers
         end
