4cd252906584b54367c3378fbf78e42373906275
Varpusparvi <mikko.karkee1@gmail.com>
2024-07-09 15:58:05 +0200

    Make CSP more strict

--- examples/demo/config/initializers/content_security_policy.rb
+++ examples/demo/config/initializers/content_security_policy.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
     policy.img_src :self, :https, :data
     policy.object_src :none
     policy.script_src :self, :https, :strict_dynamic
-    policy.style_src :self, :https, :unsafe_inline
+    policy.style_src :self, :https
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
   end
